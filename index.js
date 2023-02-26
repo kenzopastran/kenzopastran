670047427f98aa95f2852383ef87a55de3f88d84
Kenzo Pastran <123145874+kenzopastran@users.noreply.github.com>
2023-02-25 23:09:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("iconoinsta").onclick = function() {
+window.open("https://www.instagram.com/kenzopastran/")
+}
+
+document.getElementById("iconoface").onclick = function() {
+    window.open("https://www.facebook.com/kenzopastran/")
+    }
+
+document.getElementById("iconowhats").onclick = function() {
+        window.open("https://api.whatsapp.com/send/?phone=5491122850676&text&type=phone_number&app_absent=0")
+        }
+
+document.getElementById("iconosound").onclick = function() {
+        window.open("https://soundcloud.com/silversteam")
+            }
+
+
+
+
+
